45130a555e9489dc8eb072e06cb9c08bb1e80773
unknown <alinenink>
2024-06-30 21:38:32 -0300

    ajuste de cores e layout

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,11 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'alinenink' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('alinenink');
-  });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
